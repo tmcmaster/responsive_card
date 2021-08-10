72cf94261440c9b505ecff8b827cdb177e9939c5
Tim McMaster <tim@mcmaster.id.au>
2021-08-10 20:15:01 +1000

    trying to fix web build

--- bin/build-web.sh
+++ bin/build-web.sh
@@ -2,7 +2,8 @@
 
 flutter clean
 flutter pub get
-flutter build web -t app/main.dart
-sed -i 's/base href="\/"/base href="\/responsive_card\/"/g' build/web/index.html
+flutter packages pub get;
+flutter build web
+sed -i 's/base href="\/"/base href="\/responsive_card\/"/g' build/web/index.html || exit 1
 echo "Web build completed successfully"
 exit 0
