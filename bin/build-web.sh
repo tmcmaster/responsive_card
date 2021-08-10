b3f7178506d2f5f6b359323ef5752a1d07228f52
Tim McMaster <tim@mcmaster.id.au>
2021-08-10 21:05:32 +1000

    fixing web build workflow

--- bin/build-web.sh
+++ bin/build-web.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-flutter clean
-flutter config --enable-web
 flutter pub get
-flutter packages pub get;
 flutter build web
-sed -i 's/base href="\/"/base href="\/responsive_card\/"/g' build/web/index.html || exit 1
-echo "Web build completed successfully"
-exit 0
